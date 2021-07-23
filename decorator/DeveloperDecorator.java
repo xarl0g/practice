e23936d9034a01bd4c5a3b7f4b605b8463a9c3be
xarl0g <87869644+xarl0g@users.noreply.github.com>
2021-07-23 19:09:45 +0300

    Add files via upload

---
+++ decorator/DeveloperDecorator.java
@@ -0,0 +1,13 @@
+package mai.decorator;
+
+public class DeveloperDecorator implements Developer{
+    Developer developer;
+
+    public DeveloperDecorator(Developer developer){
+        this.developer=developer;
+    }
+    @Override
+    public String makeJob() {
+        return developer.makeJob();
+    }
+}
